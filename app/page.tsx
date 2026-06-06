f1ae9e74239ca77cc7349ce9d2041ac5677ae45e
yoanastamenova <yoana.stamenovaa@gmail.com>
2026-06-06 11:29:24 +0200

    feat: add Proceso component to the main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Footer from "./components/footer/footer";
 import Hero from "./components/hero/hero";
 import Servicios from "./components/servicios/servicios";
 import Contacto from "./components/contacto/contacto";
+import Proceso from "./components/proceso/proceso";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
       <Servicios />
+      <Proceso />
       <Contacto />
       <Footer />
     </div>
